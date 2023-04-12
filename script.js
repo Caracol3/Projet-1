16cc0b1eab861c174cfd8f8ccbd7c796a5923a52
Missant <damien.missant@reseau.sncf.fr>
2023-04-12 15:27:20 +0200

    update

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function showResponsiveMenu() {
-    let menu = document.getElementById("topnav_responsive_menu");
-    let icon = document.getElementById("topnav_hamburger_icon");
-    let root = document.getElementById("root");
-    if (menu.className === "") {
-      menu.className = "open";
-      icon.className = "open";
-      root.style.overflowY = "hidden";
-    } else {
-      menu.className = "";                    
-      icon.className = "";
-      root.style.overflowY = "";
-    }
+  let menu = document.getElementById("topnav_responsive_menu");
+  let icon = document.getElementById("topnav_hamburger_icon");
+  let root = document.getElementById("root");
+  if (menu.className === "") {
+    menu.className = "open";
+    icon.className = "open";
+    root.style.overflowY = "hidden";
+  } else {
+    menu.className = "";
+    icon.className = "";
+    root.style.overflowY = "";
   }
+}
